99c5499cbe09ee5f3fc0f4f25c1f63f156266b91
Roavor <shpacovicho@gmail.com>
2017-09-11 22:36:28 +0300

    second commit arter fail

---
+++ EssentialTools.Tests/UnitTest2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
+using System.Linq;
+using Moq;
+
+
+namespace EssentialTools.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products = {
+            new Product {Name = "Каяк", Category = "Водные виды спорта", Price = 275M},
+            new Product {Name = "Спасательный жилет", Category = "Водные виды спорта", Price = 48.95M},
+            new Product {Name = "Мяч", Category = "Футбол", Price = 19.50M},
+            new Product {Name = "Угловой флажок", Category = "Футбол", Price = 34.95M}
+        };
+        [TestMethod]
+        public void TesSum_Products_Correctly()
+        {
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
+            var result = target.ValueProducts(products);
+            Assert.AreEqual(products.Sum(e =>e.Price), result);
+           
+        }
+        private Product[] createProduct(decimal value)
+        {
+            return new[] { new Product { Price = value } };
+        }
+        [TestMethod]
+        public void Pass_Throught_Variable_Discounts()
+        {
+            // Arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0)))
+                .Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100)))
+                .Returns<decimal>(total => total*0.9M);
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100,
+                Range.Inclusive))).Returns<decimal>(total => total-5);
+            var target = new LinqValueCalculator(mock.Object);
+            // Act
+            decimal FiveDollarDiscount = target.ValueProducts(createProduct(5));
+            decimal TenDollarDiscount = target.ValueProducts(createProduct(10));
+            decimal FiftyDollarDiscount = target.ValueProducts(createProduct(50));
+            decimal HundredDollarDiscount = target.ValueProducts(createProduct(100));
+            decimal FiveHundredDollarDiscount = target.ValueProducts(createProduct(500));
+            // Assert
+            Assert.AreEqual(5, FiveDollarDiscount, "$5 lost");
+            Assert.AreEqual(5, TenDollarDiscount, "$10 lost");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 lost");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 lost");
+            Assert.AreEqual(450, FiveHundredDollarDiscount, "$500 lost");
+            target.ValueProducts(createProduct(0));
+
+        }
+    }
+}
---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IValueCalculator calc;
+        // GET: Home
+        private Product[] products =
+        {
+            new Product { Name = "Каяк", Category = "Водные ", Price = 275M },
+            new Product { Name = "Каяк", Category = "Водные виды спорта", Price = 48.95M },
+            new Product { Name = "Каяк", Category = "Водные виды спорта", Price =  19.50M },
+            new Product { Name = "Каяк", Category = "Водные ", Price = 34.95M }
+        };
+        public HomeController()
+        {
+
+        }
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public HomeController(IValueCalculator calcParam, IValueCalculator calc2)
+        {
+            calc = calcParam;
+        }
+        public ActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart(calc);
+            cart.Products = products;
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
